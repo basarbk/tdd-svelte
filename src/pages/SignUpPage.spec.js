2d21c003bfce8259846e527d9b0f9f9681001744
Basar Buyukkahraman <basarbk@yahoo.com>
2021-04-04 01:22:17 +0300

    0201-Sign-Up-Form-Layout

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -2,8 +2,52 @@ import SignUpPage from "./SignUpPage.svelte";
 import { render, screen } from "@testing-library/svelte";
 import "@testing-library/jest-dom";
 
-it("has Sign Up header", () => {
-  render(SignUpPage);
-  const header = screen.getByRole("heading", { name: "Sign Up" });
-  expect(header).toBeInTheDocument();
+describe("Sign Up Page", () => {
+  describe("Layout", () => {
+    it("has Sign Up header", () => {
+      render(SignUpPage);
+      const header = screen.getByRole("heading", { name: "Sign Up" });
+      expect(header).toBeInTheDocument();
+    });
+    it("has username input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("Username");
+      expect(input).toBeInTheDocument();
+    });
+    it("has email input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("E-mail");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("Password");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("Password");
+      expect(input.type).toBe("password");
+    });
+    it("has password repeat input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("Password Repeat");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password repeat input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("Password Repeat");
+      expect(input.type).toBe("password");
+    });
+    it("has Sign Up button", () => {
+      render(SignUpPage);
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      expect(button).toBeInTheDocument();
+    });
+    it("disables the button initially", () => {
+      render(SignUpPage);
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      expect(button).toBeDisabled();
+    });
+  });
 });
--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -1,1 +1,10 @@
 <h1>Sign Up</h1>
+<label for="username">Username</label>
+<input id="username"/>
+<label for="e-mail">E-mail</label>
+<input id="e-mail"/>
+<label for="password">Password</label>
+<input id="password" type="password"/>
+<label for="password-repeat">Password Repeat</label>
+<input id="password-repeat" type="password"/>
+<button disabled>Sign Up</button>
